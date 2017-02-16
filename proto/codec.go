e9123f1c01412b16e1d2d89798f9a35d54e677e3
Vitaly Lobchuk <vitaly@lobchuk.ru>
2017-02-16 22:34:51 +0300

    refactor(all): big refactoring and optimization

---
+++ proto/codec.go
@@ -0,0 +1,40 @@
+package proto
+
+import (
+	"github.com/gogo/protobuf/proto"
+	"github.com/l-vitaly/observer"
+)
+
+const contentType = "application/protobuf"
+
+type Codec struct {
+}
+
+func NewCodec() *Codec {
+	return &Codec{}
+}
+
+type codecRequest struct {
+	request observer.Request
+}
+
+type codecResponse struct {
+	data proto.Message
+}
+
+func (c *Codec) NewRequest(r observer.Request) observer.CodecRequest {
+	return &codecRequest{request: r}
+}
+
+func (c *Codec) NewResponse(data interface{}) (string, observer.CodecResponse) {
+	return contentType, &codecResponse{data: data.(proto.Message)}
+}
+
+func (c *codecResponse) Body() ([]byte, error) {
+	return proto.Marshal(c.data)
+}
+
+func (c *codecRequest) ReadRequest(i interface{}) error {
+	args := i.(proto.Message)
+	return proto.Unmarshal(c.request.Body(), args)
+}
